faa38b90eeeafc45a6cb1fd36a3fe7ad79af2ceb
Blackpossum <yosaphatharwindra@gmail.com>
2024-06-11 22:49:55 +0700

    feat: migration models and versioning

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from Logging_config import setup_logging
 from Routes.tasks import tasks_bp
 from Routes.distribute_task import task_distribution_bp
@@ -12,7 +14,7 @@ import os
 
 load_dotenv()
 
-Server_uri =os.getenv('SERVER_URI')
+SECRET_URI =os.getenv('SERVER_URI')
 
 app = Flask(__name__)
 logger = setup_logging()
@@ -24,14 +26,23 @@ app.register_blueprint(worker_bp, url_prefix='/sig/worker-nodes')
 app.register_blueprint(cncCrud_bp, url_prefix='/cnc-job')
 app.register_blueprint(task_monitoring_bp, url_prefix='/sig')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = Server_uri
+# some_dev01 - 5/june/2024
+# i know it sucks and tireblly confusing to have run database in 2 place, my bad 
+# but i will fix it later
+
+# some_dev02 - 10/june/2024
+#  yeah it did 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SECRET_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     return "Hello World!"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
